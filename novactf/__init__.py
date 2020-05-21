adb7b230ca23373f21d4544586f3ed965595d808
fede-pe <fepe.ig@gmail.com>
2020-05-21 15:01:26 +0200

    auto installation of NovaCtf software

--- novactf/__init__.py
+++ novactf/__init__.py
@@ -26,7 +26,6 @@
 
 import pwem
 
-
 _logo = ""
 _references = []
 
@@ -40,3 +39,43 @@ class Plugin(pwem.Plugin):
     def getEnviron(cls):
         return None
 
+    @classmethod
+    def getDependencies(cls):
+        neededPrograms = ['git', 'fftw3', 'fftw3f', 'lib64', 'gcc']
+
+        return neededPrograms
+
+    @classmethod
+    def defineBinaries(cls, env):
+        version = '1.0.0'
+        NOVACTF_INSTALLED = 'novactf_%s_installed' % version
+        warningMsg1 = "'WARNING: if program fails when executing reinstall in scipion3/software/em/novactf-1.0.0 " \
+                      "setting the path to the include files from fftw3 as well as following fftw3 libraries: fftw3 " \
+                      "fftw3f. Typically both libraries should be in one folder and thus one library path should be " \
+                      "sufficient. Example command:'"
+        warningMsg2 = "'make includepath = \"path_to_fftw_include_files\" libpath = \"path_to_fftw_libraries\"'"
+        warningMsg3 = "'NovaCTF also requires standard libraries with the standard path being " \
+                      "/usr/lib64 - if the libraries are elsewhere open makefile and change the path accordingly'"
+
+        # Download git repo
+        installationCmd = 'git clone https://github.com/turonova/novaCTF.git && '
+
+        # Binaries compilation
+        installationCmd += 'cd novaCTF && ' \
+                           'make && ' \
+                           'cd .. && '
+
+        # Create installation finished flag file
+        installationCmd += 'touch %s && ' % NOVACTF_INSTALLED
+
+        # Display warning message
+        installationCmd += 'echo %s && ' % warningMsg1
+        installationCmd += 'echo %s && ' % warningMsg2
+        installationCmd += 'echo %s' % warningMsg3
+
+        env.addPackage('novactf',
+                       version=version,
+                       tar='void.tgz',
+                       neededProgs=cls.getDependencies(),
+                       commands=[(installationCmd, NOVACTF_INSTALLED)],
+                       default=True)
