b461d3506705bcbd15c1e72148fdb556f9e6d36a
fede-pe <fepe.ig@gmail.com>
2021-02-22 13:30:57 +0100

    remove debug code from reconstruciton porotocol

--- novactf/protocols/protocol_tomoReconstruction.py
+++ novactf/protocols/protocol_tomoReconstruction.py
@@ -69,9 +69,6 @@ class ProtNovaCtfTomoReconstruction(EMProtocol, ProtTomoBase):
     # -------------------------- INSERT steps functions ---------------------
     def _insertAllSteps(self):
 
-        print("------------------------------------------------------------------------")
-        print(self.protTomoCtfDefocus.get().numberOfIntermediateStacks[0].get())
-
         for index, ts in enumerate(self.protTomoCtfDefocus.get().inputSetOfTiltSeries.get()):
             convertInputId = self._insertFunctionStep('convertInputStep',
                                                       ts.getObjId(),
